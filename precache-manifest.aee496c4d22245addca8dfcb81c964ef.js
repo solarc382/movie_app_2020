01383d011ece0a6a98cd54c7f5711635111829d9
SolaMin <solarc382@gmail.com>
2020-08-18 23:59:22 +0900

    Updates

---
+++ precache-manifest.aee496c4d22245addca8dfcb81c964ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc663f271012447465813455e7ed0949",
+    "url": "//movie_app_2020/index.html"
+  },
+  {
+    "revision": "aac75a75d9c63516956c",
+    "url": "//movie_app_2020/static/css/main.0b68eddd.chunk.css"
+  },
+  {
+    "revision": "8b7ab456ca2673ac934a",
+    "url": "//movie_app_2020/static/js/2.09240959.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "//movie_app_2020/static/js/2.09240959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aac75a75d9c63516956c",
+    "url": "//movie_app_2020/static/js/main.f6aa0b21.chunk.js"
+  },
+  {
+    "revision": "b17a9260a65d586f21aa",
+    "url": "//movie_app_2020/static/js/runtime-main.a90dfb4a.js"
+  }
+]);
